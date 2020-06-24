16245bbe77872cb1ae9393f011201b090ef4a06d
Darbaz Ali <darbaz.a.h@gmail.com>
2020-06-24 13:18:52 +0300

    handle state with redux

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,47 +1,81 @@
 import React, { Component } from "react";
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux'
 
-export default class QuoteBox extends Component {
+
+
+const quotes = [
+    {
+      text:
+        "Genius is one percent inspiration and ninety-nine percent perspiration.",
+      author: "Thomas Edison",
+    },
+    {
+      text: "You can observe a lot just by watching.",
+      author: "Yogi Berra",
+    },
+    {
+      text: "A house divided against itself cannot stand.",
+      author: "Abraham Lincoln",
+    },
+    {
+      text: "Difficulties increase the nearer we get to the goal.",
+      author: "Johann Wolfgang von Goethe",
+    },
+    {
+      text: "Fate is in your hands and no one elses",
+      author: "Byron Pulsifer",
+    },
+    {
+      text: "Be the chief but never the lord.",
+      author: "Lao Tzu",
+    },
+    {
+      text: "Nothing happens unless first we dream.",
+      author: "Carl Sandburg",
+    },
+    {
+      text: "Well begun is half done.",
+      author: "Aristotle",
+    },
+]
+
+
+
+const NEW_QUOTE = 'NEW_QUOTE';
+const newQuoteAction = () => {
+    return {
+        type: NEW_QUOTE
+    }
+}
+
+
+const getRandomItem = array => array[Math.floor( Math.random() * array.length )]
+
+const reducer = (state = [], action ) => {
+    const randomItem = getRandomItem(quotes);
+
+    switch ( action.type ) {
+        case NEW_QUOTE:
+            return {
+                text: randomItem.text,
+                author: randomItem.author
+            }
+        default: return state;
+    }
+}
+
+const store = createStore(reducer);
+
+
+
+
+class Presentational extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quotes: [
-        {
-          text:
-            "Genius is one percent inspiration and ninety-nine percent perspiration.",
-          author: "Thomas Edison",
-        },
-        {
-          text: "You can observe a lot just by watching.",
-          author: "Yogi Berra",
-        },
-        {
-          text: "A house divided against itself cannot stand.",
-          author: "Abraham Lincoln",
-        },
-        {
-          text: "Difficulties increase the nearer we get to the goal.",
-          author: "Johann Wolfgang von Goethe",
-        },
-        {
-          text: "Fate is in your hands and no one elses",
-          author: "Byron Pulsifer",
-        },
-        {
-          text: "Be the chief but never the lord.",
-          author: "Lao Tzu",
-        },
-        {
-          text: "Nothing happens unless first we dream.",
-          author: "Carl Sandburg",
-        },
-        {
-          text: "Well begun is half done.",
-          author: "Aristotle",
-        },
-      ],
-      text: "",
-      author: "",
+      quotes
     };
 
     this.handleNewQuote = this.handleNewQuote.bind(this);
@@ -57,21 +91,11 @@ export default class QuoteBox extends Component {
     //         jsonData = [...data]
     //     });
 
-    const randomIndex = Math.floor(Math.random() * this.state.quotes.length);
-    const randomItem = this.state.quotes[randomIndex];
-    this.setState((state) => ({
-      text: randomItem.text,
-      author: randomItem.author,
-    }));
+    this.props.getNewQuote(this.state)
   }
 
   handleNewQuote() {
-    const randomIndex = Math.floor(Math.random() * this.state.quotes.length);
-    const randomItem = this.state.quotes[randomIndex];
-    this.setState((state) => ({
-      text: randomItem.text,
-      author: randomItem.author,
-    }));
+    this.props.getNewQuote(this.state)
   }
 
   render() {
@@ -79,9 +103,9 @@ export default class QuoteBox extends Component {
 
         <div id="quote-box">
             <span id="quote-mark">"</span>
-          <h3 id="text">{this.state.text}</h3>
+          <h3 id="text">{this.props.quotes.text}</h3>
 
-          <p id="author">{this.state.author}</p>
+          <p id="author">{this.props.quotes.author}</p>
 
             <div id="box-buttons">
             <a href="twitter.com/intent/tweet" id="tweet-quote">
@@ -97,4 +121,37 @@ export default class QuoteBox extends Component {
 
     );
   }
+}
+
+
+
+const mapStateToProps = ( state ) => {
+    return {
+        quotes: state
+    }
+}
+
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getNewQuote: function() {
+            dispatch(newQuoteAction());
+        }
+    }
+}
+
+
+
+const Container = connect( mapStateToProps, mapDispatchToProps)(Presentational);
+
+
+
+export default class QuoteBox extends Component {
+    render() {
+        return (
+           <Provider store={store}>
+               <Container />
+           </Provider>
+        )
+    }
 }
