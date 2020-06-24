f05b1f47dd4932076e81cf495072cd1bf261d4b5
Darbaz Ali <darbaz.a.h@gmail.com>
2020-06-24 12:02:05 +0300

    design done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import QuoteBox from './QuoteBox'
+import QuoteBox from "./QuoteBox";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello React!</h1>
       <QuoteBox />
     </div>
   );
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,96 +1,100 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class QuoteBox extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            quotes: [
-                {
-                    "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
-                    "author": "Thomas Edison"
-                  },
-                  {
-                    "text": "You can observe a lot just by watching.",
-                    "author": "Yogi Berra"
-                  },
-                  {
-                    "text": "A house divided against itself cannot stand.",
-                    "author": "Abraham Lincoln"
-                  },
-                  {
-                    "text": "Difficulties increase the nearer we get to the goal.",
-                    "author": "Johann Wolfgang von Goethe"
-                  },
-                  {
-                    "text": "Fate is in your hands and no one elses",
-                    "author": "Byron Pulsifer"
-                  },
-                  {
-                    "text": "Be the chief but never the lord.",
-                    "author": "Lao Tzu"
-                  },
-                  {
-                    "text": "Nothing happens unless first we dream.",
-                    "author": "Carl Sandburg"
-                  },
-                  {
-                    "text": "Well begun is half done.",
-                    "author": "Aristotle"
-                  }
-            ],
-            text: "",
-            author: ""
-        }
+  constructor(props) {
+    super(props);
 
-        this.handleNewQuote = this.handleNewQuote.bind(this)
-    }
+    this.state = {
+      quotes: [
+        {
+          text:
+            "Genius is one percent inspiration and ninety-nine percent perspiration.",
+          author: "Thomas Edison",
+        },
+        {
+          text: "You can observe a lot just by watching.",
+          author: "Yogi Berra",
+        },
+        {
+          text: "A house divided against itself cannot stand.",
+          author: "Abraham Lincoln",
+        },
+        {
+          text: "Difficulties increase the nearer we get to the goal.",
+          author: "Johann Wolfgang von Goethe",
+        },
+        {
+          text: "Fate is in your hands and no one elses",
+          author: "Byron Pulsifer",
+        },
+        {
+          text: "Be the chief but never the lord.",
+          author: "Lao Tzu",
+        },
+        {
+          text: "Nothing happens unless first we dream.",
+          author: "Carl Sandburg",
+        },
+        {
+          text: "Well begun is half done.",
+          author: "Aristotle",
+        },
+      ],
+      text: "",
+      author: "",
+    };
 
-    UNSAFE_componentWillMount() {
-        // let jsonData;
-        //  fetch("https://type.fit/api/quotes")
-        //     .then(function(response) {
-        //         return response.json();
-        //     })
-        //     .then(function(data) {
-        //         jsonData = [...data]
-        //     });
+    this.handleNewQuote = this.handleNewQuote.bind(this);
+  }
 
-        const randomIndex = Math.floor( Math.random() * this.state.quotes.length );
-        const randomItem = this.state.quotes[randomIndex];
-        this.setState(state => ({
-            text: randomItem.text,
-            author: randomItem.author
-        }))
+  UNSAFE_componentWillMount() {
+    // let jsonData;
+    //  fetch("https://type.fit/api/quotes")
+    //     .then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(data) {
+    //         jsonData = [...data]
+    //     });
 
-    }
+    const randomIndex = Math.floor(Math.random() * this.state.quotes.length);
+    const randomItem = this.state.quotes[randomIndex];
+    this.setState((state) => ({
+      text: randomItem.text,
+      author: randomItem.author,
+    }));
+  }
 
-    handleNewQuote() {
-        const randomIndex = Math.floor( Math.random() * this.state.quotes.length );
-        const randomItem = this.state.quotes[randomIndex];
-        this.setState(state => ({
-            text: randomItem.text,
-            author: randomItem.author
-        }))
-    }
-    
-    render() {
+  handleNewQuote() {
+    const randomIndex = Math.floor(Math.random() * this.state.quotes.length);
+    const randomItem = this.state.quotes[randomIndex];
+    this.setState((state) => ({
+      text: randomItem.text,
+      author: randomItem.author,
+    }));
+  }
 
-       
+  render() {
+    return (
 
+        <div id="quote-box">
+            <span id="quote-mark">"</span>
+          <h3 id="text">{this.state.text}</h3>
 
-        return (
-            <div id="quote-box">
+          <p id="author">{this.state.author}</p>
 
-                <h3 id="text">"{this.state.text}</h3>
+            <div id="box-buttons">
+            <a href="twitter.com/intent/tweet" id="tweet-quote">
+                        Tweet Quote
+                    </a>
 
-                <p id="author">{this.state.author}</p>
-
-                <button id="new-quote" onClick={this.handleNewQuote}>New Quote</button>
-
-                <a href="twitter.com/intent/tweet" id="tweet-quote">Tweet Quote</a>
-
+                    <button className="btn" id="new-quote" onClick={this.handleNewQuote}>
+                        New Quote
+                    </button>
             </div>
-        )
-    }
+         
+        </div>
+
+    );
+  }
 }
