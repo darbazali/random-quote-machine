458ed837967a5e6cd2957af35417dfa8f1aed593
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-09 14:53:04 +0300

    code format

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,48 +1,42 @@
-
-import React, { useState, useEffect} from 'react'
-import defaultQuots from './defaultQuots'
+import React, { useState, useEffect } from "react";
+import defaultQuots from "./defaultQuots";
 
 const QuoteBox = () => {
-
   const getRandomItem = (array) =>
-  array[Math.floor(Math.random() * array.length)];
+    array[Math.floor(Math.random() * array.length)];
 
   const [quotes, setQuotes] = useState(defaultQuots);
-  const [quote, setQuote] = useState( getRandomItem(quotes))
+  const [quote, setQuote] = useState(getRandomItem(quotes));
 
-  
-  useEffect( () => {
-      const url = "https://type.fit/api/quotes";
-      fetch( url )
-      .then( res => res.json())
-      .then( data => setQuotes(data))
+  useEffect(() => {
+    const url = "https://type.fit/api/quotes";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setQuotes(data));
+  }, []);
 
-  }, [])
+  function handleNewQuote() {
+    setQuote(getRandomItem(quotes));
+  }
 
+  return (
+    <div id="quote-box">
+      <span id="quote-mark">"</span>
+      <h3 id="text">{quote.text}</h3>
 
-    function handleNewQuote() {
-        setQuote( getRandomItem(quotes))
-    }
-      
+      <p id="author">- {quote.author}</p>
 
-    
-   return (
-     <div>
-        <h1>{quote.text}</h1>
-        <p>{quote.author}</p>
-        <button onClick={handleNewQuote}>New Quote</button>
-     </div>
-   );
-}
+      <div id="box-buttons">
+        <a href="#" id="tweet-quote">
+          Tweet Quote
+        </a>
+
+        <button className="btn" id="new-quote" onClick={handleNewQuote}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default QuoteBox;
-
-
-
-
-
-  
-
-
-
-
